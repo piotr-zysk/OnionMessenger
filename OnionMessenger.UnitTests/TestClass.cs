cdb487550d2b675f05f4064124d177c018f7c9d4
Piotr Zysk <pizy123@gmail.com>
2019-03-23 10:43:16 +0100

    Added Xunit with first (dummy) unit test.

---
+++ OnionMessenger.UnitTests/TestClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace OnionMessenger.UnitTests
+{
+    public class TestClass
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+            Assert.Equal(3, Add(1, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(3, 2));
+        }
+
+        private int Add(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+    }
+}
