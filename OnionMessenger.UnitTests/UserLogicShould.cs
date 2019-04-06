7bee6ff8eadb307d774d0f9a4972163b11a4ac11
Piotr Zysk <pizy123@gmail.com>
2019-04-06 15:41:00 +0200

    Another (newer) way to return async method in Moq.

--- OnionMessenger.UnitTests/UserLogicShould.cs
+++ OnionMessenger.UnitTests/UserLogicShould.cs
@@ -20,7 +20,10 @@ namespace OnionMessenger.UnitTests
             UserRepository.Setup(x => x.GetByLogin(It.IsAny<string>())).Returns(new User() { Id = 2, Login = "user1" });
             UserRepository.Setup(x => x.GetByLogin("user1")).Returns(new User() { Id = 1, Login = "user1" });
 
-            UserRepository.Setup(x => x.GetByLoginAsync(It.IsAny<string>())).Returns(Task.FromResult(new User() { Id = 2, Login = "user1" }));
+            //compatible with all Moq versions:
+            //UserRepository.Setup(x => x.GetByLoginAsync(It.IsAny<string>())).Returns(Task.FromResult(new User() { Id = 2, Login = "user1" }));
+            // new Method in Moq 4.2:
+            UserRepository.Setup(x => x.GetByLoginAsync(It.IsAny<string>())).ReturnsAsync(new User() { Id = 2, Login = "user1" });
 
             var userRepository = new Lazy<IUserRepository>(() => UserRepository.Object);
 
